4528ab2e7c07029c820d4a69f08c589ac4eb3fd3
soeunb <b_soeun_@naver.com>
2021-10-07 22:21:49 +0900

    import/export js

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,8 +1,8 @@
-<script type='text/javascript' src='../config.js'></script>
-const WEATHER_API_KEY = WEATHER_API.KEY;
+import {WEATHER_API} from '/config.js';
+const WEATHER_API_KEY = WEATHER_API;
 
-function onGeoOK(position){
-    
+function onGeoOK(position) {
+
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
@@ -17,7 +17,7 @@ function onGeoOK(position){
         });
 }
 
-function onGeoError(){
+function onGeoError() {
     // error
 }
 
